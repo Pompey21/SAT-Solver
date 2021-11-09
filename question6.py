e6762f9669eacff257bbc92f3f357a73b40d9a9a
Marko Mekjavic <s1813308@seadart.inf.ed.ac.uk>
2021-11-09 16:53:48 +0000

    Question 6 done!

--- question6.py
+++ question6.py
@@ -3,12 +3,17 @@ from functools import reduce
 """
     Z3 takes as input simple-sorted formulas that may contain symbols with pre-defined meanings defined by a theory.
 """
+# in order to play around with the model, please just change the number of employees and the number of shifts
+# which are defined as integer variables here:
+
 number_employees = 9
-number_shifts = 21
+number_shifts = 18
 
 solver = Solver()
 
-# generating the variables
+# generating the variables, where each variable is defined as employee i doing shift j
+# this is then stored in a variable of the form x_i_j which will be True if employee i
+# is indeed taking shift j.
 variables = []
 for j in range(number_shifts):
     shifts = []
@@ -18,8 +23,8 @@ for j in range(number_shifts):
     variables.append(shifts)
 
 # set of employees
-employees = range(9)
-shifts = range(21)
+employees = range(number_employees)
+shifts = range(number_shifts)
 
 # a) There are only 2 employees per shift
 # adding variables to solver as a restricted integers (either 1 or 0)
@@ -35,9 +40,12 @@ for j in shifts:
 
 
 # b) no back-2-back shifts
+# here we check that no two consequtive variables (= denoting the shifts with j and employees with i)
+# are True -> looking at consequtive shifts of the same employee!
+# The wrap around case is handled by the modulo function!
 for j in shifts:
     for i in employees:
-        solver.add( Or(variables[j][i] == 0, variables[(j+1)%21][i] == 0))
+        solver.add( Or(variables[j][i] == 0, variables[(j+1)%number_shifts][i] == 0))
 
 
 # # c) Check that all are doing Equal Shifts
